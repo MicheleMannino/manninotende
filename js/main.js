b24a5afa37db8076b7f8db6f73ce332b15755119
michele.mannino <michele.mannino@accenture.com>
2016-02-20 19:11:38 +0100

    Added new scroll to top button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-$(document).click(function (event) {
-    var target = $(event.target);
-    var $navbar = $(".navbar-collapse");
-    var _opened = $navbar.hasClass("in");
-    if (_opened === true && !target.hasClass("navbar-toggle")) {
-        $navbar.collapse('hide');
+(function() {
+    $(document).click(function (event) {
+        var target = $(event.target);
+        var $navbar = $(".navbar-collapse");
+        var _opened = $navbar.hasClass("in");
+        if (_opened === true && !target.hasClass("navbar-toggle")) {
+            $navbar.collapse('hide');
+        }
+    });
+
+    if (($(window).height() + 100) < $(document).height()) {
+        $('#top-link-block').removeClass('hidden').affix({
+            offset: {top:100}
+        }).on('click', function() {
+            $('html,body').animate({scrollTop:0},'slow');
+            return false;
+        });
     }
-});
+})();
